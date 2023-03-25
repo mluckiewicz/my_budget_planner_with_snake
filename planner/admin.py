5c6c3d9d75dbc6b08234d66168f9ed49c0973fea
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-03-25 15:20:41 +0100

    add(admin.py): models were registered in  admin panel with a dedicated table layout

--- planner/admin.py
+++ planner/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Categories, Transactions, Types, Frequencies, RepeatableTransactions
 
-# Register your models here.
+
+@admin.register(Types)
+class TypesAdmin(admin.ModelAdmin):
+    list_display = ("type_name",)
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = (
+        "category_name",
+        "type",
+    )
+
+
+@admin.register(Transactions)
+class TransactionsAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "category",
+        "description",
+        "amount",
+        "transaction_type",
+        "is_repatable",
+        "created",
+        "updated",
+    )
+
+
+@admin.register(Frequencies)
+class FrequenciesAdmin(admin.ModelAdmin):
+    list_display = (
+        "frequency_name",
+    )
+    
+    
+@admin.register(RepeatableTransactions)
+class RepeatableTransactionsAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "start_date",
+        "end_date",
+        "transaction_type",
+        "category",
+        "frequency",
+        
+    )
