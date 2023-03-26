0e4ad3f4b525fc3ce1f6546b5a0815817b670ae6
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-03-27 00:10:47 +0200

    add(models.py): Update models to v1.2

--- planner/models.py
+++ planner/models.py
@@ -1,76 +1,90 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
 
 
 class Types(models.Model):
-    
     # Fields
     type_name = models.CharField(max_length=20, unique=True)
-    
+
     # Methods
     def __str__(self):
         return self.type_name
 
-    
+
 class Categories(models.Model):
-    
     # Fields
-    category_name = models.CharField(max_length=255, blank=False)
+    category_name = models.CharField(max_length=255, null=False)
     type = models.ForeignKey(Types, on_delete=models.CASCADE)
-    
+
     # Methods
     def __str__(self):
         return self.category_name
 
 
-
-
-class Frequencies(models.Model):
-    
+class Budgets(models.Model):
     # Fields
-    frequency_name = models.CharField(max_length=255)
-    
+    budget_name = models.CharField(max_length=255, null=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    start_date = models.DateField(default=timezone.now, null=False)
+    end_date = models.DateField(default=timezone.now, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     # Methods
     def __str__(self):
-        return self.frequency_name
+        return self.budget_name
 
 
 class RepeatableTransactions(models.Model):
-    
-    # fields
+    # Choises for recurrence_type field
+    class RecurrenceTypes(models.TextChoices):
+        DAILY = "DAILY", _("Daily")
+        WEEKLY = "WEEKLY", _("Weekly")
+        MONTHLY = "MONTHLY", _("Monthly")
+        QUATERLY = "QUATERLY", _("Quaterly")
+        ANNUALLY = "ANNUALLY", _("Annually")
+
+    # Fields
     description = models.CharField(max_length=255)
-    start_date = models.DateField(default=timezone.now)
-    end_date = models.DateField(default=timezone.now)
-    num_occurrences = models.IntegerField(default=1)
-    frequency_days = models.IntegerField(default=1)
+    base_amout = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    start_date = models.DateField(default=timezone.now, null=False)
+    end_date = models.DateField(default=timezone.now, null=False)
+    recurrence_type = models.CharField(
+        max_length=10,
+        choices=RecurrenceTypes.choices,
+        default=RecurrenceTypes.DAILY,
+        null=False,
+    )
+    recurrence_value = models.IntegerField(default=1, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    frequency = models.ForeignKey(Frequencies, on_delete=models.CASCADE)
-    transaction_type = models.ForeignKey(Types, on_delete=models.CASCADE)
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, default=None)
+    type = models.ForeignKey(Types, on_delete=models.CASCADE, default=None)
+    budget = models.ForeignKey(Budgets, on_delete=models.CASCADE, default=None)
+
     # Methods
     def __str__(self):
         return self.description
-    
-    
-class Transactions(models.Model):
 
+
+class Transactions(models.Model):
     # Fields
     description = models.CharField(max_length=255, blank=False, null=False)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
-    is_repatable = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    transaction_type = models.ForeignKey(Types, on_delete=models.CASCADE)
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    repeatable_transaction = models.ForeignKey(RepeatableTransactions, on_delete=models.CASCADE)
+    amount = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=False, null=False
+    )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     execution_date = models.DateField(auto_now=True)
     is_executed = models.BooleanField(default=False)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, default=None)
+    type = models.ForeignKey(Types, on_delete=models.CASCADE, default=None)
+    repeatable_transaction = models.ForeignKey(
+        RepeatableTransactions, null=True, on_delete=models.CASCADE
+    )
+    budget = models.ForeignKey(Budgets, on_delete=models.CASCADE)
+
     # Methods
     def __str__(self):
         return self.description
-
-    
