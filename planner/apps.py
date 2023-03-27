223098d8975fcdf07ca675f57b6efde2ca496387
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-03-27 17:14:09 +0200

    add(signals.py): Add logic to create multiple records

--- planner/apps.py
+++ planner/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PlannerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'planner'
+
+    def ready(self) -> None:
+        import planner.signals
+        
---
+++ planner/signals.py
@@ -0,0 +1,36 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import RepeatableTransactions, Transactions
+import datetime
+
+
+@receiver(post_save, sender=RepeatableTransactions)
+def create_repeatable_transactions(sender, instance, created, **kwargs):
+    """ 
+    Catches a signal to add a new record to the Recurring Transactions table. Based 
+    on the number of repetitions, adds the appropriate number of records to the 
+    Transactions table.
+    """
+    if created:
+        num_repeats = instance.recurrence_value
+        for _ in range(num_repeats):
+            # Calculate execution date
+            
+            
+            
+            # Add new record 
+            transaction = Transactions.objects.create(
+                description = instance.description,
+                amount = instance.base_amout,
+                execution_date = datetime.datetime.now().date(),
+                user = instance.user,
+                category = instance.category,
+                type = instance.type,
+                repeatable_transaction = instance,
+                budget = instance.budget
+            )
+            transaction.save()
+            
+            
+ # Signal registration           
+post_save.connect(create_repeatable_transactions, sender=RepeatableTransactions)
