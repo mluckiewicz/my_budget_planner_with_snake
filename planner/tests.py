d379d97aa9d6cbd00e8975516d06eb546751209c
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-03-29 20:12:00 +0200

    test(planner): add test to date_generator.py

--- planner/tests.py
+++ planner/tests.py
@@ -1,3 +1,54 @@
+import datetime
 from django.test import TestCase
-
+from .utils.date_generator import DateCalculator
 # Create your tests here.
+
+class TestDateCalculator(TestCase):
+    def setUp(self):
+        self.start_date = datetime.date(2022, 1, 1)
+        self.end_date = datetime.date(2022, 3, 31)
+        self.dc = DateCalculator(self.start_date, self.end_date)
+
+    def test_generate_daily_dates(self):
+        dates = self.dc._generate_daily_dates()
+        self.assertEqual(len(dates), (self.end_date - self.start_date).days + 1)
+
+    def test_generate_weekly_dates(self):
+        recurrence_value = 0 # Monday
+        dates = self.dc._generate_weekly_dates(recurrence_value)
+        for date in dates:
+            self.assertEqual(date.weekday(), recurrence_value)
+
+    def test_generate_monthly_dates(self):
+        recurrence_value = 15 # 15th day of the month
+        dates = self.dc._generate_monthly_dates(recurrence_value)
+        self.assertEqual(len(dates), 3) # should generate 3 dates
+        self.assertEqual(dates[0], self.start_date.replace(day=recurrence_value)) # first date should be the start date with the day replaced by the recurrence value
+        for i in range(1, len(dates)):
+            self.assertEqual(dates[i].day, recurrence_value) # all other dates should have the recurrence value as the day
+
+    def test_generate_quaterly_dates(self):
+        # TODO add test for quarterly dates
+        pass
+
+    def test_get_dates_daily(self):
+        dates = self.dc.get_dates('daily')
+        self.assertEqual(len(dates), (self.end_date - self.start_date).days + 1)
+
+    def test_get_dates_weekly(self):
+        recurrence_value = 0 # Monday
+        dates = self.dc.get_dates('weekly', recurrence_value)
+        for date in dates:
+            self.assertEqual(date.weekday(), recurrence_value)
+
+    def test_get_dates_monthly(self):
+        recurrence_value = 15 # 15th day of the month
+        dates = self.dc.get_dates('monthly', recurrence_value)
+        self.assertEqual(len(dates), 3) # should generate 3 dates
+        self.assertEqual(dates[0], self.start_date.replace(day=recurrence_value)) # first date should be the start date with the day replaced by the recurrence value
+        for i in range(1, len(dates)):
+            self.assertEqual(dates[i].day, recurrence_value) # all other dates should have the recurrence value as the day
+
+    def test_get_dates_quaterly(self):
+        # TODO add test for quarterly dates
+        pass
