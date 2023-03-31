ec57ad4d60e1f216ad2c4afb5e782fb253633d80
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-03-31 17:41:38 +0200

    refactor(urls.py): format with black

--- planner/urls.py
+++ planner/urls.py
@@ -4,13 +4,28 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('dashboard/',views.dashboard,name='dashboard'),
-    path('add_transaction_single/',views.add_transaction_single,name='add_transaction_single'),
-    path('add_transaction_repeatable/', views.add_transaction_repeatable, name='add_transaction_repeatable'),
-    path('edit_transaction_single/', views.edit_transaction_single, name='edit_transaction_single'),
-    path('edit_transaction_repeatable/', views.edit_transaction_repeatable, name='edit_transaction_repeatable'),
-    path('search/', views.search, name='search'),
-    path('calendar_view/', views.calendar_view, name='calendar_view'),
-
+    path("", views.home, name="home"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path(
+        "add_transaction_single/",
+        views.add_transaction_single,
+        name="add_transaction_single",
+    ),
+    path(
+        "add_transaction_repeatable/",
+        views.add_transaction_repeatable,
+        name="add_transaction_repeatable",
+    ),
+    path(
+        "edit_transaction_single/",
+        views.edit_transaction_single,
+        name="edit_transaction_single",
+    ),
+    path(
+        "edit_transaction_repeatable/",
+        views.edit_transaction_repeatable,
+        name="edit_transaction_repeatable",
+    ),
+    path("search/", views.search, name="search"),
+    path("calendar_view/", views.calendar_view, name="calendar_view"),
 ]
