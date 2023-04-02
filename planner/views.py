d65d6061c10b289960d54da402fe465f2e70c203
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-02 15:38:34 +0200

    refactor(views.py): change to class based url and change endpoint name

--- planner/views.py
+++ planner/views.py
@@ -1,32 +1,41 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 from .forms import SingleTransactionForm
+from .models import Transaction
 
-from django.contrib import messages
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def home(request):
-    return redirect("login") # -> accounts.login
+    return redirect("login")  # -> accounts.login
 
 
 def dashboard(request):
     return render(request, "base.html")
 
 
-def add_transaction_single(request):
-    if request.method == "POST":
-        form = SingleTransactionForm(request.POST)
-        if form.is_valid():
-            pass
-    else:
-        form = SingleTransactionForm()
-    return render(
-        request,
-        "transaction/add_transaction_single.html",
-        context={"form": form},
-    )
+class AddSingleTransactionView(FormView):
+    form_class = SingleTransactionForm
+    template_name = "transaction/add_transaction_single.html"
+    success_url = "/dashboard/"
+
+    def form_valid(self, form):
+        current_user = self.request.user
+
+        Transaction.objects.create(
+            user=current_user,
+            type=form.cleaned_data["type"],
+            amount=form.cleaned_data["amount"],
+            category=form.cleaned_data["category"],
+            budget=form.cleaned_data["budget"],
+            execution_date=form.cleaned_data["execution_date"],
+            is_executed=form.cleaned_data["is_executed"],
+            description=form.cleaned_data["description"],
+        )
+        return super().form_valid(form)
 
 
 def add_transaction_repeatable(request):
