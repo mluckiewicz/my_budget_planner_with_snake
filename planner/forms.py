ff416408c69f07972ef35c6785c39648b4a9335a
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-02 15:32:44 +0200

    refactor(forms.py): chenge ModelForm to Form

--- planner/forms.py
+++ planner/forms.py
@@ -1,28 +1,50 @@
-from django.forms import ModelForm, Select
+from django import forms
 from django.utils.translation import gettext_lazy as _
+from djmoney.forms.fields import MoneyField
+from .models import Transaction, Type, Category, Budget
 
-from .models import Transaction
 
+class SingleTransactionForm(forms.Form):
+    currency_choices = (
+        ("PLN", "PLN"),
+        ("USD", "USD"),
+        ("EUR", "EUR"),
+    )
 
-class SingleTransactionForm(ModelForm):
-    class Meta:
-        model = Transaction
-        fields = (
-            'description',
-            'amount',
-            'execution_date',
-            'is_executed',
-            'category',
-            'type',
-            'budget'
-        )
-        labels = {
-            'type': _('Typ'),
-            'category': _('Kategoria'),
-            'budget': _('Budżet'),
-        }
-        widgets = {
-            'type': Select(attrs={'class': 'form-select'}),
-            'category': Select(attrs={'class': 'form-select'}),
-            'budget': Select(attrs={'class': 'form-select'}),
-        }
+    type = forms.ModelChoiceField(
+        queryset=Type.objects.all(),
+        required=True,
+        label=_("Typ"),
+    )
+    amount = MoneyField(
+        currency_widget=forms.widgets.Select(
+            attrs={"class": "form-select"},
+            choices=currency_choices,
+        ),
+        min_value=0,
+        max_digits=10,
+        decimal_places=2,
+        default_amount=0,
+        default_currency="PLN",
+    )
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        required=True,
+        label=_("Kategoria"),
+    )
+    budget = forms.ModelChoiceField(
+        queryset=Budget.objects.all(),
+        required=True,
+        label=_("Budżet"),
+    )
+    execution_date = forms.DateField(
+        required=True,
+        label=_("Data realizacji"),
+    )
+    is_executed = forms.BooleanField(
+        required=False,
+        label=_("Zrealizowa?"),
+    )
+    description = forms.CharField(
+        required=False, widget=forms.Textarea(attrs={"rows": "3"})
+    )
