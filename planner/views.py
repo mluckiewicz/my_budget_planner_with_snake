58f8ade49c4fe328e779396de78c202925c88507
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-02 18:59:12 +0200

    add(views.py): add repeatabletransaction route

--- planner/views.py
+++ planner/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
-from .forms import SingleTransactionForm
-from .models import Transaction
+from .forms import AddSingleTransactionForm, AddRepeatableTransactionForm
+from .models import Transaction, RepeatableTransaction
 
 
 # Create your views here.
@@ -18,7 +18,7 @@ def dashboard(request):
 
 
 class AddSingleTransactionView(FormView):
-    form_class = SingleTransactionForm
+    form_class = AddSingleTransactionForm
     template_name = "transaction/add_transaction_single.html"
     success_url = "/dashboard/"
 
@@ -33,6 +33,29 @@ class AddSingleTransactionView(FormView):
             budget=form.cleaned_data["budget"],
             execution_date=form.cleaned_data["execution_date"],
             is_executed=form.cleaned_data["is_executed"],
+            description=form.cleaned_data["description"],
+        )
+        return super().form_valid(form)
+
+
+class AddRepeatableTransactionView(FormView):
+    form_class = AddRepeatableTransactionForm
+    template_name = "transaction/add_transaction_repeatable.html"
+    success_url = "/dashboard/"
+
+    def form_valid(self, form):
+        current_user = self.request.user
+
+        RepeatableTransaction.objects.create(
+            user=current_user,
+            type=form.cleaned_data["type"],
+            base_amout=form.cleaned_data["base_amout"],
+            start_date=form.cleaned_data["start_date"],
+            end_date=form.cleaned_data["end_date"],
+            recurrence_type=form.cleaned_data["recurrence_type"],
+            recurrence_value=form.cleaned_data["recurrence_value"],
+            category=form.cleaned_data["category"],
+            budget=form.cleaned_data["budget"],
             description=form.cleaned_data["description"],
         )
         return super().form_valid(form)
