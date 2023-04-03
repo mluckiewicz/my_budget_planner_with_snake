24791a46b48692132feb8cc5fdbd0cbcae4c495a
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-03 22:00:48 +0200

    refactor(urls.py): change login and logout  patterns to LoginView and LogoutView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
-from . import views
 from django.contrib.auth import views as auth_views
+from . import views
 
 
 urlpatterns = [
-    path('login/', views.login_page, name='login'),
-    path('logout/', views.logout_page, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name="account/login.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="account/logout.html"), name='logout'),
     path('register/', views.register_page, name='register'),
-
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"), name="password_reset_complete"),
-
 ]
