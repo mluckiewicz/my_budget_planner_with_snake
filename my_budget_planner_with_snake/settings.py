4d809690c0e7529475d265de3ab5af95c4229482
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-03 21:54:23 +0200

    add(settings.py): add login parameters

--- my_budget_planner_with_snake/settings.py
+++ my_budget_planner_with_snake/settings.py
@@ -127,3 +127,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'planner:dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
