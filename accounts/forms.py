1eb9d7e7631d69f6d96f901afc6fc270251cd92c
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-03 21:55:25 +0200

    refactor(forms.py): remove redundant imports

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
+
 
 class CreateUserForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
