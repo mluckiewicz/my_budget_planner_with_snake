e4fba45e27e84d1a4a5d492ab31525acd5e20738
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-03 18:38:25 +0200

    refactor(urls.py): add app name in urls pattenrs

--- my_budget_planner_with_snake/urls.py
+++ my_budget_planner_with_snake/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('planner.urls')),
-    path('', include('accounts.urls')),
+    path('planner/', include('planner.urls')),
+    path('account/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
