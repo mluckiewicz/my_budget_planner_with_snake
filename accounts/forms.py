033439b88b42f89b52963c0daf57de4634162230
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-04 21:16:53 +0200

    refactor(forms.py): change register form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,17 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from django.contrib.auth.models import User
 
-
-class CreateUserForm(UserCreationForm):
-    class Meta(UserCreationForm):
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    
+    class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ('username', 'first_name', 'email')
         
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Hasła nie są identyczne.')
+        return cd['password2']
+            
