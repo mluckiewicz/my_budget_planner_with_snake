27929aba6bad7c4d3fd3db238bfaadb95784fc1d
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-04 19:05:20 +0200

    refactor(urls.py): change template path in accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="account/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name="account/logout.html"), name='logout'),
     path('register/', views.register_page, name='register'),
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"), name="password_reset_complete"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="account/password_reset.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="account/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="account/password_reset_done.html"), name="password_reset_complete"),
 ]
