d94fd3657023d96a8d7d19c262f6b0313cc16912
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-06 22:33:16 +0200

    refactor(models.py): remove redundant import

--- planner/models.py
+++ planner/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 from django.utils import timezone
 from djmoney.models.fields import MoneyField
-from djmoney.models.validators import MaxMoneyValidator, MinMoneyValidator
+from djmoney.models.validators import MinMoneyValidator
 
 
 class Type(models.Model):
