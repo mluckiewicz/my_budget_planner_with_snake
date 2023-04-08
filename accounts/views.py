99564f6d0da28bfb6dcaa7ecebe00c11874f3dda
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-08 08:41:58 +0200

    add(views.py): add AuthRedirectView and AuthLoginView

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import RedirectView
+from django.contrib.auth.views import LoginView
 from .forms import UserRegistrationForm
 
 
@@ -13,3 +16,48 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, "account/register.html", {"form": form})
+
+
+class AuthRedirectView(RedirectView):
+    """
+    Redirects authenticated users to the dashboard, otherwise to the login page.
+
+    If the user is authenticated, this view will redirect to the dashboard page
+    using the URL defined by the 'planner:dashboard' named URL pattern.
+
+    If the user is not authenticated, this view will call the 'get_redirect_url'
+    method of the parent class to get the redirect URL. By default, this will
+    redirect to the URL defined by the 'login' named URL pattern.
+    """
+
+    def get_redirect_url(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return reverse_lazy('planner:dashboard')
+        else:
+            return super().get_redirect_url(*args, **kwargs)
+        
+        
+class AuthLoginview(LoginView):
+    """
+    Displays the login page and logs the user in if valid credentials are submitted.
+
+    This view is based on Django's built-in 'LoginView' class and inherits all of
+    its functionality, including form validation, authentication, and redirecting.
+
+    By default, this view will use the 'account/login.html' template and will
+    redirect authenticated users to the dashboard URL defined by the 'planner:dashboard'
+    named URL pattern. This behavior can be customized by setting the 'template_name',
+    'redirect_authenticated_user', and 'success_url' attributes.
+
+    Attributes:
+        template_name (str): The name of the template to use for the login page.
+            Defaults to 'account/login.html'.
+        redirect_authenticated_user (bool): Whether to redirect authenticated
+            users to the success URL instead of the login page. Defaults to True.
+        success_url (str): The URL to redirect to after successful login.
+            Defaults to the URL defined by the 'planner:dashboard' named URL pattern.
+    """
+    template_name = 'account/login.html'
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('planner:dashboard')
+    
