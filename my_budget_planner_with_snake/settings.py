a7f32013093057d28a00b7177b58baa25b2690c1
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-12 19:27:08 +0200

    refator(settings.py): install categories app

--- my_budget_planner_with_snake/settings.py
+++ my_budget_planner_with_snake/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'planner.apps.PlannerConfig',
+    'categories.apps.CategoriesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
