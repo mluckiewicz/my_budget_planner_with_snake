b83e416a9fd69cc25cedd1f91918a2ccab54b686
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-12 18:32:04 +0200

    refactor(base.html): move base.html outside applications

--- my_budget_planner_with_snake/settings.py
+++ my_budget_planner_with_snake/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'my_budget_planner_with_snake.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
