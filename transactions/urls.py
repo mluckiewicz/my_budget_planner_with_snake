900c62a5f7ffc09ea94c69f3eafabcc47129354a
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-13 22:09:39 +0200

    feat(urls.py): add delete and edit url patterns

--- transactions/urls.py
+++ transactions/urls.py
@@ -16,8 +16,18 @@ urlpatterns = [
         name="add_single",
     ),
     path(
+        "delete_transactions/",
+        views.delete_transactions,
+        name="delete_transactions",
+    ),
+    path(
         "add_repeatable/",
         views.AddRepeatableTransactionView.as_view(),
         name="add_repeatable",
+    ),
+    path(
+        "edit/<int:pk>/",
+        views.TransactionUpdateView.as_view(),
+        name="edit_transaction",
     ),
 ]
