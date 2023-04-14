ad9bdfbdc79c38e758c12cc814eced6520f3b0bd
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-14 21:12:15 +0200

    fix(BUG_005): add logic to check if recur day is grater then 30

--- transactions/utils/date_generator.py
+++ transactions/utils/date_generator.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import datetime
-from dateutil.relativedelta import relativedelta
+import calendar
 
 
 class DateCalculator:
@@ -47,7 +47,9 @@ class DateCalculator:
         ]
         return dates
 
-    def _generate_monthly_dates(self, recurrence_value: int|None = None) -> list[datetime.date]:
+    def _generate_monthly_dates(
+        self, recurrence_value: int | None = None
+    ) -> list[datetime.date]:
         """
         Generates a list of dates in monthly intervals between the start date and the end date.
 
@@ -55,28 +57,47 @@ class DateCalculator:
         :return: A list of datetime.date objects representing the dates in monthly intervals.
         """
         dates = []
-        if recurrence_value is not None:
-            current_date = self.start_date.replace(day=recurrence_value)
-        else:
-            current_date = self.start_date
-            
+        current_date = (
+            self.start_date.replace(day=recurrence_value)
+            if recurrence_value
+            else self.start_date
+        )
+
         while current_date <= self.end_date:
             dates.append(current_date)
+            
+            # Period is over 12 months
             if current_date.month == 12:
-                current_date = current_date.replace(year=current_date.year+1, month=1)
+                current_date = current_date.replace(year=current_date.year + 1, month=1)
+                
             else:
-                current_date = current_date.replace(month=current_date.month+1)
+                if current_date.month + 1 == 2 and recurrence_value > 28:
+                    current_date = current_date.replace(
+                        month=current_date.month + 1, day=28
+                    )
+
+                elif recurrence_value == 31:
+                    current_date = current_date.replace(
+                        month=current_date.month + 1,
+                        day=calendar.monthrange(
+                            current_date.year, current_date.month + 1
+                        )[1],
+                    )
+                else:
+                    current_date = current_date.replace(month=current_date.month + 1)
         return dates
 
     #! NIE DZIAŁA ZGODNIE Z ZAŁOŻENIEM
     def _generate_quaterly_dates(self) -> list[datetime.date]:
-        # TODO Need to write function to calculate quater duration 
+        # TODO Need to write function to calculate quater duration
         days_per_qtr = {1: 91, 2: 91, 3: 92, 4: 92}
-        
+
         duration = (self.end_date - self.start_date).days
         dates = list([self.start_date])
         current_quarter = (self.start_date.month - 1) // 3 + 1
-        next_date = self.start_date + datetime.timedelta(days=days_per_qtr.get(current_quarter))
+        next_date = self.start_date + datetime.timedelta(
+            days=days_per_qtr.get(current_quarter)
+        )
 
         while next_date <= self.start_date + datetime.timedelta(days=duration):
             dates.append(next_date)
@@ -86,7 +107,7 @@ class DateCalculator:
 
     def _generate_annually_dates(self, recurrence_value: int) -> list[datetime.date]:
         pass
-    
+
     def get_dates(
         self, unit: str, recurrence_value: int | None = None
     ) -> list[datetime.date]:
