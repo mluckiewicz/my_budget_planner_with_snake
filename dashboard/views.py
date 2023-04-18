5b5cec0359b9a62b12ca9b7248a685c42a6c3317
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-18 21:14:49 +0200

    refactor(views.py): add class View and data function to work wit calendar

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,35 @@
 from __future__ import annotations
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import View
+from transactions.models import Transaction
+from django.http import JsonResponse
+from transactions.models import Transaction
 
 
-@login_required
-def dashboard(request):
-    return render(request, "base.html")
+@method_decorator(login_required, name="dispatch")
+class DashboardView(View):
+    template_name = "dashboard/view.html"
+
+    def get(self, request):
+        data = Transaction.objects.all()
+        
+        return render(request, self.template_name)
+    
+
+def data(request):
+    transactions = Transaction.objects.all()
+    data = []
+    for transaction in transactions:
+        data.append({
+            'title': transaction.category.category_name,
+            'start': transaction.execution_date.isoformat(),
+            'amount': {
+                'amount': str(transaction.amount.amount),
+                'currency': str(transaction.amount.currency)
+            },
+        })
+    return JsonResponse(data, safe=False)
+    
+    
