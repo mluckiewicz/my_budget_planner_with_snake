5b5765b3195b5fff4ac4e369948386a2fdb4e631
mluckiewicz <mateuszluckiewicz@gmail.com>
2023-04-18 21:15:22 +0200

    add(settings.py): install dashboard app

--- my_budget_planner_with_snake/settings.py
+++ my_budget_planner_with_snake/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'dashboard.apps.DashboardConfig',
     'categories.apps.CategoriesConfig',
     'budgets.apps.BudgetsConfig',
     'transactions.apps.TransactionsConfig',
